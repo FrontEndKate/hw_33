5e08a7ea399abcb03b778c66265b8724f7db794e
Ekaterina Goncharenko <eg93006@gmail.com>
2024-10-30 07:02:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import './App.css';
-import Game from "./components/Game";
-import Result from "./components/Result";
-import Start from "./components/Start";
 import React from 'react';
 
 const App = () => {
-    const [activePage, setActivePage] = React.useState('start');
-    const [player, setPlayer] = React.useState('');
-    const [gameRes, setGameRes] = React.useState([0,0]);
-
-    const changePage = page => setActivePage( page );
-    const changeName = name => setPlayer(name);
-    const setResult = res => setGameRes(res);
-
-    switch (activePage) {
-        case 'game':
-            return <Game changePage={changePage} player={player}
-                         setResult={setResult}/>
-        case 'result':
-            return <Result changePage={changePage} gameRes={gameRes}/>
-        default:
-            return <Start changePage={changePage} changeName={changeName}/>
-    }
+    return (
+        <div>
+            
+        </div>
+    );
 };
 
 export default App;
